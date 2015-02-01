cf1c00bd5e1f4e2ab584801396c84e16de56cff0
Bert Muthalaly <bert@somethingdoneright.net>
2015-02-01 13:46:36 -0500

    d'oh! s/update-in/om\/transact! app is working :D

--- src/pokerforecast/core.cljs
+++ src/pokerforecast/core.cljs
@@ -15,7 +15,7 @@
                                            {:name "Nick"
                                             :rsvpd 1
                                             :attended 1}]
-                               :index 0
+                               :index 0 ; TODO: use map-indexed at call site instead 
                                :hidden true}
                               {:date "Tuesday, January 19"
                                :attending [{:name "Bert"
@@ -24,7 +24,7 @@
                                            {:name "Max"
                                             :rsvpd 1
                                             :attended 1}]
-                               :index 0
+                               :index 1
                                :hidden true}]}))
 
 (defn render-date
@@ -47,38 +47,42 @@
   (apply dom/ul #js {:className (str "attendees" (if (:hidden game) " hide" ""))}
          (map render-attendee (:attending game))))
 
+(def toggle-chan (chan))
+
+(put! toggle-chan "toggle-chan is working")
+(go
+  (println (<! toggle-chan)))
+
 (defn game-view 
   [game owner]
   (reify
     om/IRenderState
     (render-state [this state]
-      (let [toggle-chan (:toggle-chan state)] ; TODO: s/let/destructuring  
-        (dom/li nil
-          (render-date game) 
-          (render-attending-count game)
-          (dom/button #js {:onClick (fn [e] 
-                                      (println "Click triggered!") 
-                                      (put! toggle-chan 0))} 
-                      "Show attending")
-          (render-attendees game))))))
+      (dom/li nil
+        (render-date game) 
+        (render-attending-count game)
+        (dom/button #js {:onClick (fn [e] 
+                                    (println "Click triggered!") 
+                                    (put! toggle-chan (:index game)))} 
+                    "Show attending")
+        (render-attendees game)))))
 
 (defn render-game-list
   [app owner]
   (reify
-    om/IInitState
-    (init-state [_]
+    #_om/IInitState
+    #_(init-state [_]
       (println "state initialized")
       {:toggle-chan (chan)})
     om/IWillMount
     (will-mount [_]
-      (let [toggle-chan (om/get-state owner :toggle-chan)]
-        (go 
-          (loop []
-            (println "go-loop started")
-            (let [toggled-game-index (<! toggle-chan)]
-              (println (str "Index is " toggled-game-index))
-              (update-in app [:games toggled-game-index :hidden] not))
-            (recur)))))
+      (go 
+        (loop []
+          (println "go-loop started")
+          (let [toggled-game-index (<! toggle-chan)]
+            (println (str "Index is " toggled-game-index))
+            (om/transact! app [:games toggled-game-index :hidden] not))
+          (recur))))
     om/IRenderState
     (render-state [this state]
       (apply dom/ul nil 
