61c115a63188783b99ac17b2ee116ede31b90dea
Bert Muthalaly <bert@somethingdoneright.net>
2015-02-04 11:29:35 -0500

    too complicated and probably no fun

--- src/pokerforecast/core.cljs
+++ src/pokerforecast/core.cljs
@@ -8,6 +8,8 @@
 
 (enable-console-print!)
 
+; TODO: consider using a map of game-id -> game instead of a
+; vector, and simply using sort-by :date to render/re-render
 (def app-state (atom {:games [{:date "Monday, January 18"
                                 :attending [{:name "James"
                                              :rsvpd 3
@@ -25,43 +27,56 @@
                                              :attended 1}]
                                 :hidden true}]}))
 
-(defn render-date
+(defn date-html
   [game]
   (dom/span #js {:className "date"}
             (:date game)))
 
-(defn render-attending-count
+(defn attending-count-html
   [game]
   (dom/span #js {:className "attending"}
     (count (:attending game))))
 
-(defn render-attendee
+(defn attendee-html
   [attendee]
   (dom/li nil (:name attendee)))
 
-(defn render-attendees
+(defn attendees-html
   [game]
   ; TODO: check goog/css for classname manipulation utils
   (apply dom/ul #js {:className (str "attendees" (if (:hidden game) " hide" ""))}
-         (map render-attendee (:attending game))))
+         (map attendee-html (:attending game))))
+
+
+(defn show-button-html
+  [toggle-chan]
+  (fn [game]
+    (dom/button #js {:onClick (fn [e] 
+                                (put! toggle-chan (:index game)))} 
+                "Show attending")))
+
+(defn game-html 
+  [{:keys [toggle-chan]}]
+  (fn [game]
+    (juxt 
+      date-html 
+      attending-count-html 
+      (show-button-html toggle-chan) 
+      attendees-html))
+  
 
 (defn game-view 
   [game owner]
   (reify
     om/IRenderState
-    (render-state [this {:keys [toggle-chan]}]
-      (dom/li nil
-        (render-date game) 
-        (render-attending-count game)
-        (dom/button #js {:onClick (fn [e] 
-                                    (put! toggle-chan (:index game)))} 
-                    "Show attending")
-        (render-attendees game)))))
+    (render-state [this {:keys [channels] :as channels}]
+      (apply dom/li nil
+             ((game-html channels) game))))
 
 (defn- assoc-with-indices [coll]
   (map-indexed (fn [i item] (assoc item :index i)) coll))
 
-(defn render-game-list
+(defn game-list-view
   [app owner]
   (reify
     om/IInitState
@@ -81,9 +96,9 @@
       (apply dom/ul nil 
             (om/build-all game-view 
                           (assoc-with-indices (:games app))
-                          {:init-state {:toggle-chan toggle-chan}})))))
+                          {:init-state {:channels {:toggle-chan toggle-chan}}})))))
 
 (om/root 
-  render-game-list
+  game-list-view
   app-state
   {:target (. js/document getElementById "app")})
