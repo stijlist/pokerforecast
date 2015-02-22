07c842f001fa283912b8d7e16a98803a660a5eef
Bert Muthalaly <bert@somethingdoneright.net>
2015-02-22 14:31:46 -0500

    args on same line as defn

--- src/pokerforecast/core.cljs
+++ src/pokerforecast/core.cljs
@@ -2,19 +2,16 @@
 
 (enable-console-print!)
 
-(defn- attendance-rate
-  [{:keys [attended rsvpd]}]
+(defn- attendance-rate [{:keys [attended rsvpd]}]
   (if (= 0 rsvpd) nil (/ attended rsvpd)))
 
-(defn- powerset
-  [coll]
+(defn- powerset [coll]
   (if-not (seq coll) 
     [[]]
     (let [rest-powerset (powerset (rest coll))]
       (concat (map #(conj % (first coll)) rest-powerset) rest-powerset))))
 
-(defn- game-likelihood
-  [attendees]
+(defn- game-likelihood [attendees]
   (->> attendees
        (map attendance-rate) ; what do we do with unknown attendance rates?
        (filter (comp not nil?))
@@ -28,8 +25,7 @@
 (defn- default [d input]
   (if (nil? input) d input))
 
-(defn maximum-game-likelihood 
-  [attending]
+(defn maximum-game-likelihood [attending]
   (->> (powerset attending) ; all combinations of folks that might show up
        (filter enough-players)
        (map game-likelihood)
